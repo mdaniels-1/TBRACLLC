27b849e5940259f452e978b188bc84b9cb4e047f
mdaniels-1 <gymnast8910@gmail.com>
2024-05-19 01:41:34 -0400

    home page mini modifications

--- tbracllc/src/pages/Home.jsx
+++ tbracllc/src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import '../../public/home.css'
 
 const Home = () => {
   return (
-    <div>Home Page</div>
+    <div>
+      <h1 className='title'>TBRACLLC</h1>
+      <h2 className='mini_title'>Insert mini intro here. like 'architecture, hvac, blah, and blah.'</h2>
+    </div>
   )
 }
 
