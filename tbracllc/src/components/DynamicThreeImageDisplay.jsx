f3be749662b80be1bcb339fcbdd8041b9f1a9058
mdaniels-1 <gymnast8910@gmail.com>
2024-06-05 20:08:44 -0400

    home page - video display

---
+++ tbracllc/src/components/DynamicThreeImageDisplay.jsx
@@ -0,0 +1,14 @@
+
+const DynamicThreeImageDisplay = ({img1_src, img1_text, img2_src, img2_text, img3_src, img3_text}) =>{
+    return(
+        <div className='home_images_container'>
+            <img id='img1' src={img1_src} alt={img1_text}/>
+            <img id='img2' src={img2_src} alt={img2_text}/>
+            <img id='img3' src={img3_src} alt={img3_text}/>
+        </div>
+    )
+}
+
+export default DynamicThreeImageDisplay;
+
+
