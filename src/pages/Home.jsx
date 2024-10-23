f8e36ebce4c6e00a1321dd9a2c92798470f9e112
mdaniels-1 <127329331+mdaniels-1@users.noreply.github.com>
2024-10-22 23:00:04 -0400

    remove comments in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,8 +19,8 @@ const Home = () => {
 
         <div className='title_overlay'>
           <h1 className='title'>TBRAC LLC.</h1>
-          <p> need to adjust icon or the text so it&apos;s centered. have to check that</p>
-          <p className='mini_title'>Insert mini intro here. like &apos;architecture, hvac, blah, and blah.&apos;</p>
+{/*           <p> need to adjust icon or the text so it&apos;s centered. have to check that</p> */}
+{/*           <p className='mini_title'>Insert mini intro here. like &apos;architecture, hvac, blah, and blah.&apos;</p> */}
         </div>
       </div>
 
